3f3eb01322083d50ef5cbc9190f05519387bb157
Arnaud Rosette <arnaud.rosette@outlook.com>
2014-10-28 19:29:51 +0100

    Modification du main pour etre compatible avec le jar

--- more/Main.java
+++ more/Main.java
@@ -9,25 +9,31 @@ import java.util.TreeSet;
 public class Main {
     
     public static void main (String[] args) throws FileNotFoundException, IOException, SyntaxErrorException {
-        LexicalAnalyzer scanner = null;
-            
-        scanner = new LexicalAnalyzer(new java.io.FileReader(args[0])); 
-            
-        Symbol symbol = null;
-        do {
-            symbol = scanner.nextToken();
-            if (symbol.getType() != LexicalUnit.END_OF_STREAM) {
-                System.out.println(symbol);
-            }
-        } while(symbol.getType() != LexicalUnit.END_OF_STREAM);
-        System.out.println("Identifiers");
+    
+        if (args.length != 2) {
+            System.out.println("L'utilisation est : java -jar compiler.jar Main Iuliusfile");
+        }
+        else {
+            LexicalAnalyzer scanner = null;
+                
+            scanner = new LexicalAnalyzer(new java.io.FileReader(args[1])); 
+                
+            Symbol symbol = null;
+            do {
+                symbol = scanner.nextToken();
+                if (symbol.getType() != LexicalUnit.END_OF_STREAM) {
+                    System.out.println(symbol);
+                }
+            } while(symbol.getType() != LexicalUnit.END_OF_STREAM);
+            System.out.println("Identifiers");
 
-        HashMap<Object, Integer> identifiersMap = scanner.getIdentifiersMap();
-        TreeSet<Object> keys = new TreeSet<Object>(identifiersMap.keySet());
-        Iterator it = keys.iterator();
-        while (it.hasNext()) {
-            Object identifier = it.next();
-            System.out.println(identifier + "  " + identifiersMap.get(identifier));
+            HashMap<Object, Integer> identifiersMap = scanner.getIdentifiersMap();
+            TreeSet<Object> keys = new TreeSet<Object>(identifiersMap.keySet());
+            Iterator it = keys.iterator();
+            while (it.hasNext()) {
+                Object identifier = it.next();
+                System.out.println(identifier + "  " + identifiersMap.get(identifier));
+            }
         }
     }
 }
