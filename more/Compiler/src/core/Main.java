de745710a290b3542da8f94bcf9372b2cdd3a16d
Arnaud Rosette <arnaud.rosette@outlook.com>
2014-12-18 15:34:08 +0100

    Modification de la grammaire pour permettre que la dernière instruction d'une liste d'instructions ne soit pas suivie d'un END_OF_INSTRUCTION.

--- more/Compiler/src/core/Main.java
+++ more/Compiler/src/core/Main.java
@@ -21,13 +21,9 @@ import utils.datastructure.Variable;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        GrammarParser gp = new GrammarParser("../../more/grammar/iulius-grammar-without-func-v10.grammar");
+        GrammarParser gp = new GrammarParser("../grammar/iulius-grammar-with-func-v2.grammar");
         Grammar grammar = gp.generateGrammar();
-        //FirstFollow algo = new FirstFollow(grammar);
-        //algo.process();
-        //algo.printFF("./out.tex");
         ActionTable at = new ActionTable(grammar);
-        at.writTable("out.tex");
-        //StackParser sp = new StackParser("/home/arnaud/Documents/ulb/ma1-2014-2015/compiler/projet/compilo/test/testType.il", at);
+        StackParser sp = new StackParser("../../test/testType.il", at);
     }
 }
